b205dcec44423456f7f9d3e260e1bfe7d7f7c0bb
Saloni Vinod Alladwar <salonialladwar6@gmail.com>
2026-02-26 13:12:20 +0530

    Initial commit

---
+++ Demo/src/test/java/Demo1/Test1.java
@@ -0,0 +1,13 @@
+package Demo1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" ");
+	}
+public static void name() {
+	System.out.println("hii");
+	
+}
+}
