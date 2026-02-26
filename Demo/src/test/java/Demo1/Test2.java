e8d80d8195b8dda2e304614dc412d4ef10412252
Saloni Vinod Alladwar <salonialladwar6@gmail.com>
2026-02-26 13:24:12 +0530

    Second Commit

--- Demo/src/test/java/Demo1/Test2.java
+++ Demo/src/test/java/Demo1/Test2.java
@@ -4,7 +4,9 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Welcome to test Automation");
 	}
-
+public void name() {
+	System.out.println("Day 1");
+}
 }
